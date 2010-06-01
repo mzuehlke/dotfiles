ce89e64baca652a2386e2fd5451a0485bc832397
Marco Zuehlke <Marco.Zuehlke@brockmann-consult.de>
2010-06-01 21:51:59 +0200

    first commit

---
+++ aliases.bash
@@ -0,0 +1,8 @@
+
+
+alias gs='git status'
+alias gpr='git pull --rebase'
+alias gl="git log --graph --pretty=format:'%C(bold red)%d%Creset %s %C(bold blue)<%an> %Cgreen(%cr)%Creset' --date=relative"
+alias glg="git log --graph --pretty=format:'%C(bold red)%d%Creset %s %C(bold blue)<%an> %Cgreen(%cr)%Creset' --date=relative --all"
+alias mgs='mgit-status'
+alias mgf='mgit-fetch'
---
+++ colors.bash
@@ -0,0 +1,10 @@
+RED='\e[0;31m'
+LIGHT_RED='\e[1;31m'
+BLUE='\e[0;34m'
+LIGHT_BLUE='\e[1;34m'
+CYAN='\e[0;36m'
+LIGHT_CYAN='\e[1;36m'
+GREEN='\e[0;32m'
+LIGHT_GREEN='\e[1;32m'
+YELLOW='\e[0;33m'
+NC='\e[0m' # No Color 
