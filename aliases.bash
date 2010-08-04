0affc5ca3e53f9cec29e23b55515f61e23793172
Marco Zuehlke <Marco.Zuehlke@brockmann-consult.de>
2010-08-04 07:32:21 +0200

    fix open alias

--- aliases.bash
+++ aliases.bash
@@ -1,10 +1,17 @@
 
 
-alias open="kde-open 2> /dev/null > /dev/null"
+#alias open="kde-open 2> /dev/null > /dev/null"
+function open {
+  if [ -z "$1" ]; then
+    kde-open 2> /dev/null > /dev/null .
+  else
+    kde-open 2> /dev/null > /dev/null $1
+  fi
+}
 # git
 alias gs='git status'
 alias gpr='git pull --rebase'
 alias gl="git log --graph --pretty=format:'%C(bold red)%d%Creset %s %C(bold blue)<%an> %Cgreen(%cr)%Creset' --date=relative"
-alias glg="git log --graph --pretty=format:'%C(bold red)%d%Creset %s %C(bold blue)<%an> %Cgreen(%cr)%Creset' --date=relative --all"
+alias gla="git log --graph --pretty=format:'%C(bold red)%d%Creset %s %C(bold blue)<%an> %Cgreen(%cr)%Creset' --date=relative --all"
 alias mgs='mgit-status'
 alias mgf='mgit-fetch'
