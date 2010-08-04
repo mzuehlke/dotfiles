31d5788de3920c83442246c53ec7ece4349f967e
Marco Zuehlke <Marco.Zuehlke@brockmann-consult.de>
2010-08-04 15:53:05 +0200

    gld alias

--- aliases.bash
+++ aliases.bash
@@ -11,6 +11,7 @@ function open {
 # git
 alias gs='git status'
 alias gpr='git pull --rebase'
+alias gld='git log --decorate --oneline'
 alias gl="git log --graph --pretty=format:'%C(bold red)%d%Creset %s %C(bold blue)<%an> %Cgreen(%cr)%Creset' --date=relative"
 alias gla="git log --graph --pretty=format:'%C(bold red)%d%Creset %s %C(bold blue)<%an> %Cgreen(%cr)%Creset' --date=relative --all"
 alias mgs='mgit-status'
